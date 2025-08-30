5775299ef7f77984ef4a09f7d74e6eb792441a68
Bhattnishu <nishantbhatt2017@gmail.com>
2025-08-30 22:42:10 +0530

    Update views.py

--- learning_users/basic_app/views.py
+++ learning_users/basic_app/views.py
@@ -12,6 +12,7 @@ def user_login(request):
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
+
         user = authenticate(username=username, password=password)
 
         if user:
@@ -26,6 +27,7 @@ def user_login(request):
             return HttpResponse("Invalid login details supplied!")
     else:
         return render(request, 'basic_app/login.html', {})
+
 
 
 @login_required
